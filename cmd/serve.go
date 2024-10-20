ee795be96bbd51fb328fbbedb9e22a96205b20d7
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-10-20 18:50:25 -0400

    Add server markdown editor

--- cmd/serve.go
+++ cmd/serve.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -24,8 +25,18 @@ var serveCmd = &cobra.Command{
 
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			filename := filepath.Join(vaultDir, r.URL.Path)
+			ext := strings.ToLower(filepath.Ext(filename))
+			render := false
 
 			data, err := os.ReadFile(filename)
+			if err != nil && os.IsNotExist(err) && ext == ".html" {
+				filename = filename[:len(filename)-len(ext)] + ".md"
+				ext = ".md"
+				render = true
+
+				data, err = os.ReadFile(filename)
+			}
+
 			if err != nil {
 				if os.IsNotExist(err) {
 					http.NotFound(w, r)
@@ -36,11 +47,18 @@ var serveCmd = &cobra.Command{
 				return
 			}
 
-			ext := filepath.Ext(filename)
-
-			if ext == ".md" || ext == ".MD" {
+			if render {
 				fmt.Fprint(w, "<html><body>")
 				renderer.Render(data, w)
+				fmt.Fprint(w, "</body></html>")
+			} else if ext == ".md" {
+				fmt.Fprint(w, "<html><body>")
+				fmt.Fprint(w, "<head>")
+				fmt.Fprint(w, `<link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">`)
+				fmt.Fprint(w, `<script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>`)
+				fmt.Fprint(w, "</head>")
+				fmt.Fprint(w, "<textarea></textarea>")
+				fmt.Fprintf(w, "<script>var editor = new SimpleMDE(); editor.value(%q); </script>", data)
 				fmt.Fprint(w, "</body></html>")
 			} else {
 				w.Write(data)
